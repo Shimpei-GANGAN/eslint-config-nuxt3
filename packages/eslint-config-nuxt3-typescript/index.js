199821061392bbe83357489aecbf27e18bd9c5ac
gangan <gangan.education@gmail.com>
2023-02-19 21:03:29 +0900

    add: Prettierプラグイン追加

--- packages/eslint-config-nuxt3-typescript/index.js
+++ packages/eslint-config-nuxt3-typescript/index.js
@@ -4,14 +4,23 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ["@nuxtjs/eslint-config-typescript"],
+  extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
+  plugins: ['unused-imports'],
   rules: {
     // https://eslint.vuejs.org/rules/no-unused-properties.html#vue-no-unused-properties
-    "vue/no-unused-properties": [
-      "error",
+    'vue/no-unused-properties': [
+      'error',
       {
-        groups: ["props", "setup"],
+        groups: ['props', 'setup'],
       },
     ],
+    'unused-imports/no-unused-imports': 'error',
   },
+  overrides: [
+    {
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
 };
